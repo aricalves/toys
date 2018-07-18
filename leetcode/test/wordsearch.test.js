a48e89c7fbcda89899471b220551607e10005601
Aric Alves <aric.alves@yahoo.com>
2018-07-18 14:37:22 -0700

    Adds author provided tests

--- leetcode/test/wordsearch.test.js
+++ leetcode/test/wordsearch.test.js
@@ -2,8 +2,24 @@ const { expect } = require("chai")
 const _ = require("../wordsearch")
 
 describe("Wordsearch", () => {
-  it('Should work', () => {
-    
-    expect(true)
+  
+  const board = [
+    ['A', 'B', 'C', 'E'],
+    ['S', 'F', 'C', 'S'],
+    ['A', 'D', 'E', 'E']
+  ]
+
+  it('should find word \'ABCCED\'', () => {
+    const got = _.exist(board, 'ABCCED')
+    expect(got).to.be.true
   })
+  it('should find word \'SEE\'', () => {
+    const got = _.exist(board, 'SEE')
+    expect(got).to.be.true
+  })
+  it('should not find a word that does not exist', () => {
+    const got = _.exist(board, 'ABCB')
+    expect(got).to.be.false
+  })
+
 })
